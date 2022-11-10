3f84e6f903a1a2c926dc35dabe37e5d34d4c4620
Nabadarshi Chakma <nabadarshi@faqi.com.au>
2022-11-10 17:08:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// top-search function starts here
+
+$(document).ready(function () {
+    $(".search-by-suburb").click(function () {
+        $(".hidden-search-by-suburb").show();
+    });
+});
+
+
+
+
+
+// ends-here
