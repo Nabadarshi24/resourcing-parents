c41e966f3f3150d3514fc721193e889ec143bdaa
Kumaresh Chandra Baruri <kumaresh@faqinteractive.com.au>
2022-11-17 17:19:13 +0600

    Main search added

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // top-search function starts here
 
-$(document).ready(function () {
-    $(".search-by-suburb").click(function () {
-        $(".hidden-search-by-suburb").show();
-    });
+// $(document).ready(function () {
+//     $(".search-by-suburb").click(function () {
+//         $(".hidden-search-by-suburb").show();
+//     });
+// });
+
+$('.hamburger').click(function(){
+    $(this).toggleClass("click");
+    $('.side-menu').toggleClass("show");
 });
 
 
+$('.show-btn1').click(function(){
+    $(".side-menu ul .sub-menu1").toggleClass(("show1"));
+    $(this).toggleClass(("rotate"));
+});
 
+$('.show-btn2').click(function(){
+    $(".side-menu ul .sub-menu2").toggleClass(("show2"));
+    $(this).toggleClass(("rotate"));
+});
 
+$('.show-btn3').click(function(){
+    $(".side-menu ul .sub-menu3").toggleClass(("show3"));
+    $(this).toggleClass(("rotate"));
+});
 
+$('.show-btn4').click(function(){
+    $(".side-menu ul .sub-menu4").toggleClass(("show4"));
+    $(this).toggleClass(("rotate"));
+});
 // ends-here
